22913a04c88719bf8cc8da58892d930ea9c42cf8
Stefon Simmons <stefonsimmons@Stefons-MacBook-Air.local>
2020-04-08 00:49:30 -0400

    added datepicker 1/5

--- js/script.js
+++ js/script.js
@@ -31,17 +31,9 @@ const cardImg = document.querySelectorAll(".card-img")
 
 cardImg.forEach(c => {
   console.log(c.getAttribute("value")) //printing all card values
-  const activeCard = c.addEventListener("click", getCardValue) 
+  let activeCard = c.addEventListener("click", getCardValue) 
 })
 
-// const list1 = [[2, 3, 5, 6], [3, 6, 7, 8, 9], [1, 8, 7, 8]]
-
-// list1.forEach(e1 => {
-//   // console.log(e1)
-//   e1.forEach(e2 => {
-//     console.log (e2)
-//   })
-// })
 
 function getCardValue(e) {
   cardValue = e.target.getAttribute("value") // e.target represents the clicked element
@@ -61,6 +53,8 @@ async function showPermits(activeCard){
     const permitData = results.data
     console.log(permitData) // printing an array of specific borough/card data for every card 
 
+    activeCard = activeCard.replace(" ", "-") // for Staten Island
+    
     const film = permitData.filter(boroughPermits => boroughPermits.category === 'Film' ).length //play around with the === and == and = //
     console.log(film)
     const boroughFilms = document.querySelector(`.${activeCard}-Film`)
@@ -86,9 +80,6 @@ async function showPermits(activeCard){
     console.log(other)
     const boroughOther = document.querySelector(`.${activeCard}-other`)
     boroughOther.innerHTML = other
-
-    console.log(film + television + theater)
-
 
   } catch (error){
       console.log(`Oops! There was an error: ${error}`)
